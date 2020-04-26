7dabb85e92d658bd7f1b00e859157294f6d0d959
Vamsi Charan Adari <adarivamsi@gmail.com>
2020-04-26 07:21:11 -0400

    Added SelectionSort.

--- SortingAlgorithms/src/com/adarivamsi/SelectionSort.java
+++ SortingAlgorithms/src/com/adarivamsi/SelectionSort.java
@@ -6,7 +6,34 @@ package com.adarivamsi;
  * Created by adari on 4/25/2020
  */
 public class SelectionSort {
+
+    /** Algorithm
+         1. Start
+         2. Read the array in arr
+         3. Find the minimum element in arr
+         4. Place it at the beginning by swapping
+         5. Repeat Step 3 and 4 until the array is sorted
+     **/
+
     public int[] sort(int[] arr) {
+
+        int min_index;
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            min_index  = i;
+
+            for (int j = i+1; j < arr.length; j++) {
+                if (arr[j] < arr[min_index])
+                    min_index = j;
+            }
+
+            int temp = arr[min_index];
+            arr[min_index] = arr[i];
+            arr[i] = temp;
+        }
+
         return arr;
+
     }
+
 }
