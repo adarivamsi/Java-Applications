fe330d78298a415012483bfefb37b333ffb3da01
Vamsi Charan Adari <adarivamsi@gmail.com>
2020-04-25 23:10:41 -0400

    Created new project Sorting Algorithms.

---
+++ SortingAlgorithms/src/com/adarivamsi/SelectionSort.java
@@ -0,0 +1,12 @@
+package com.adarivamsi;
+
+/**
+ * All Copyrights reserved @ Vamsi Charan Adari
+ * Any queries mail me at adarivamsi@gmail.com
+ * Created by adari on 4/25/2020
+ */
+public class SelectionSort {
+    public int[] sort(int[] arr) {
+        return arr;
+    }
+}
